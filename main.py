34ca34f025ff071e3ad44d913b00b0882589af44
Oussama Barkouki <oussama1598@gmail.com>
2022-01-19 04:00:16 +0100

    Added: outside save file

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-# from src.games.dq_cartpole_game import DQCartpoleGame
-
-# from src.games.maze_game import MazeGame
-#from src.games.rpg_cartpole_game import RPGCartpoleGame
+from src.games.dq_cartpole_game import DQCartpoleGame
+from src.games.maze_game import MazeGame
+from src.games.rpg_cartpole_game import RPGCartpoleGame
 from src.games.sarsa_maze_game import SarsaMazeGame
 
-game = SarsaMazeGame()
-game.run(
-    verbose=True
-)
-# game.test()
+game1 = MazeGame()
+game1.run(verbose=True)
+
+game2 = DQCartpoleGame()
+game2.run(verbose=True)
+
+game3 = SarsaMazeGame()
+game3.run(verbose=True)
+
+game4 = RPGCartpoleGame()
+game4.run(verbose=True)
